88f0f62de33d5c620f4d9297a160533e6264eb3f
Lydia <lydiagashawtena@gmail.com>
2020-02-02 19:58:19 +0300

    D25 and D26 Questions

---
+++ D22/src/sample/MakeParanthesesValid.java
@@ -0,0 +1,33 @@
+package sample;
+
+import java.util.Stack;
+
+public class MakeParanthesesValid {
+    public int minAddToMakeValid(String S) {
+        char[] sArr = S.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < sArr.length; i++){
+            if(stack.isEmpty()){
+                stack.push(sArr[i]);
+                continue;
+            }
+            char top = stack.peek();
+            if(isClosing(top, sArr[i])){
+                stack.pop();
+            }else{
+                stack.push(sArr[i]);
+            }
+            System.out.println(stack.toString());
+        }
+
+        return stack.size();
+    }
+
+    boolean isClosing(char a, char b){
+        if('(' == a && ')' == b){
+            return true;
+        }
+
+        return false;
+    }
+}
